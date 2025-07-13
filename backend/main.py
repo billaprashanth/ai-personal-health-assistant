9496fae5f87b3605832d5514aaae0bfc23c43269
billaprashanth <prashanthbilla590@gmail.com>
2025-07-13 16:30:43 +0530

    changed

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import symptom_checker, reminder, chatbot, faqbot
+from backend.routers import symptom_checker, reminder, chatbot, faqbot
 from fastapi.middleware.cors import CORSMiddleware
 
 
